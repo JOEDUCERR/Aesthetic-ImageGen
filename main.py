82c419b32e3b0b3dbc4f30640ea685f1ecb4481a
JOEDUCER <joeducer.official@gmail.com>
2025-10-05 20:46:06 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import os
+from PIL import Image
+from google import genai
+from io import BytesIO
+
+def main():
+    st.set_page_config(page_title="Aesthetic ImageGen App", page_icon=":tada:", layout="wide")
+    st.title("Aesthetic ImageGen")
+    st.write("Welcome to the Aesthtic GenApp")
+
+    with st.sidebar:
+        st.header("Settings")
+        api_key = st.text_input(
+            "Enter your API key",
+            type="password",
+            key="api_key" #saves the key in session state
+        )
+        if api_key:
+            os.environ["Google_API_KEY"] = api_key
+            st.success("API key saved!")
+        else:
+            st.warning("Please enter your API key to use this app.")
+
+    prompt = st.text_area(
+        "Enter your prompt",
+        value="Make this image aesthetically pleasing...",
+        placeholder="Enter a description...",
+        height=200,
+        key="prompt"
+    )
+
+    uploaded_image = st.file_uploader(
+        "Uploaded images",
+        type=["jpg", "jpeg", 'png'],
+        key="uploaded_files"
+    )
+
+    generate_button = st.button(
+        "Generate Image",
+        key="generate_button",
+        use_container_width=True,
+        type="primary"
+    )
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.write("Uploaded Image")
+        if uploaded_image:
+            image = Image.open(uploaded_image)
+            st.image(image, caption="Uploaded Image", width="stretch")
+
+    with col2:
+        st.header("Generated Image")
+        if not generate_button:
+            st.write("Click the 'Generate Image' button to generate an image.")
+        else:
+            try:
+                with st.spinner("Generating image..."):
+                    client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
+                    image = Image.open(uploaded_image)
+                    response = client.models.generate_content(
+                        model="gemini-2.5-flash-image-preview",
+                        contents=[prompt, image]
+                    )
+                    #Tapping into only the image part of the output (not the text)
+                    image_parts = [
+                        part.inline_data.data
+                        for part in response.candidates[0].content.parts
+                        if part.inline_data
+                    ]
+                    if image_parts:
+                        image = Image.open(BytesIO(image_parts[0]))
+                        st.image(image, caption="Generated Image", width="stretch")
+                    else:
+                        st.error("Failed to generate image.")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
